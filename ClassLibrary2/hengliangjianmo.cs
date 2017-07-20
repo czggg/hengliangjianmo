7ac8f264d11b51e90d42b539efb6009bc7586ebd
958515267 <958515267@qq.com>
2017-07-20 09:05:40 +0800

    添加项目文件。

---
+++ ClassLibrary2/hengliangjianmo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB.Architecture;
+using System.IO;
+
+namespace ClassLibrary2
+{
+    [Transaction(TransactionMode.Manual)]
+    public class hengliangjianmo : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication rvtUiApp = commandData.Application;
+            Application rvtApp = rvtUiApp.Application;
+            UIDocument rvtUiDoc = rvtUiApp.ActiveUIDocument;
+            Document rvtDoc = rvtUiDoc.Document;
+
+
+
+
+
+
+
+            return Result.Succeeded;
+        }
+    }
+}
